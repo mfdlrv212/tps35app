cbf2a87bdd5ad8b146002bcd58c85756b2ef4aa0
mfdlrv212 <mafedlarosa2210@gmail.com>
2023-07-28 11:30:16 -0500

    first commit

---
+++ control.php
@@ -0,0 +1,7 @@
+<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+    <Columns>
+        <asp:BoundField DataField="Nombre" HeaderText="Nombre" />
+        <asp:BoundField DataField="Edad" HeaderText="Edad" />
+        <!-- Agrega más columnas según tu esquema de base de datos -->
+    </Columns>
+</asp:GridView>
