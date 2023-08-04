39aa8e81df9b0cc9dbbaa862c9a6186c06d41cfa
mfdlrv212 <mafedlarosa2210@gmail.com>
2023-08-04 11:54:44 -0500

    guardar

---
+++ ejercicio.js
@@ -0,0 +1,14 @@
+
+let dd = 4
+let mm = 12
+let yyyy = 2020
+
+let fecha = dd+","+mm+","+yyyy;
+
+switch (fecha){
+    case 1: 
+    
+    case 2: 
+    case 3:
+
+}
--- index.php
+++ index.php
@@ -6,12 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
+        <?php include ('comunes/titulo.php');?>
         <title>Dashboard - SB Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
+    <?php include ('comunes/sidebar/narvar.php');?>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
             <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
@@ -38,7 +40,7 @@
             </ul>
         </nav>
         <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
+            <div id="layoutSidenav_nav"> 
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
@@ -107,7 +109,6 @@
                     </div>
                 </nav>
             </div>
-            <?php include ('../includes/conn.php'); ?>
             <div id="layoutSidenav_content">
                 <main>
                    <div class="container-fluid px-4">
@@ -169,9 +170,15 @@
                                         <input type="text" class="form-control" name="rol" id="rol" required>
                                     </div>
                                 </div>
-                        </form>
                     </div>
                    </div>
+                   <br>
+                   <div class="col-lg-3">
+                    <div class="form-group">
+                        <a class="btn btn-primary" href="">Registrar Usuario</a>
+                    </div>
+                   </div>
+                   </form>
                    <br>
                         <div class="card mb-4">
                             <div class="card-header">
